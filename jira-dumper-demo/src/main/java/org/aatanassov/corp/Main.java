10ca7536bc07e4ac1b8c11027d582010b727da77
Anton Atanassov <anton.ch.atanassov@gmail.com>
2024-02-02 15:51:09 +0200

    add get comments query

--- jira-dumper-demo/src/main/java/org/aatanassov/corp/Main.java
+++ jira-dumper-demo/src/main/java/org/aatanassov/corp/Main.java
@@ -16,27 +16,40 @@ import java.util.LinkedList;
 import java.util.List;
 
 public class Main {
+    private static JiraQueryBuilder queryBuilder = new JiraQueryBuilder();
+
     public static void main(String[] args) throws IOException, URISyntaxException {
         System.out.println("Hello world!");
-        invokeGet();
+        testSearchQuery();
+        testCommentsQuery();
     }
 
-    public static void invokeGet() throws IOException, URISyntaxException {
+    private static void testCommentsQuery() throws IOException, URISyntaxException {
+        URI getCommentsQuery = queryBuilder.getCommentsQuery("JRASERVER-9091", 0, 2);
+        System.out.println("=========== executing get comments query");
+        executeGet(getCommentsQuery);
+    }
+
+    private static void testSearchQuery() throws IOException, URISyntaxException {
+        String jql = "issuetype in (Bug, Documentation, Enhancement) and updated > startOfWeek()";
+        List<String> fields = new LinkedList<>();
+        fields.add("summary");
+        fields.add("type");
+        fields.add("priority");
+        fields.add("description");
+        fields.add("reporter");
+        fields.add("created");
+
+        URI searchQuery = queryBuilder.getSearchQuery(jql, 0, 1, fields);
+        System.out.println("=========== executing search query");
+        executeGet(searchQuery);
+    }
+
+    private static void executeGet(URI getQuery) throws IOException {
         CloseableHttpClient httpClient = HttpClients.createDefault();
 
         try {
-            JiraQueryBuilder queryBuilder = new JiraQueryBuilder();
-            String jql = "issuetype in (Bug, Documentation, Enhancement) and updated > startOfWeek()";
-            List<String> fields = new LinkedList<>();
-            fields.add("summary");
-            fields.add("type");
-            fields.add("priority");
-            fields.add("description");
-            fields.add("reporter");
-            fields.add("created");
-
-            URI searchRequest = queryBuilder.getSearchQuery(jql, 0, 1, fields);
-            HttpGet request = new HttpGet(searchRequest);
+            HttpGet request = new HttpGet(getQuery);
             request.addHeader(HttpHeaders.ACCEPT, "application/json");
             CloseableHttpResponse response = httpClient.execute(request);
 
@@ -54,5 +67,6 @@ public class Main {
         } finally {
             httpClient.close();
         }
+
     }
 }
